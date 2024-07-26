5fcaee89096e1d070a29daf27493b336ae46751e
expertcoder <expertcoder6@gmail.com>
2024-07-26 17:26:41 +0530

    Adding  test case for new line

--- string_sum.rb
+++ string_sum.rb
@@ -2,12 +2,13 @@ require 'minitest/autorun'
 class StringSum
   def self.add(data)
     return 0 if data.empty?
-    data.split(',').map(&:to_i).sum
-  end
+    data.split(/,|\n/).map(&:to_i).sum
+end
 end
 puts StringSum.add("") # => 0
 puts StringSum.add("5") # => 5
 puts StringSum.add("3,5") # => 8
+puts StringSum.add("5\n4,8") # => 17
 class StringSumTest < Minitest::Test
   # Test for adding with an empty string.
   def test_addition_with_empty_string
@@ -20,6 +21,10 @@ class StringSumTest < Minitest::Test
   # Test for adding with two digit numbers.
   def test_addition_with_two_numbers
     assert_equal 6, StringSum.add("2,4")
+  end
+  # Test for adding with a mix of comma and newline separated numbers.
+  def test_addition_with_mixed_separators
+    assert_equal 23, StringSum.add("5,7\n3,8")
   end
 
 end
