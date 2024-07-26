b1f7e836f5b69cb0db36f79b928953c673026186
expertcoder <expertcoder6@gmail.com>
2024-07-26 17:24:44 +0530

    Adding test case for two number input values

--- string_sum.rb
+++ string_sum.rb
@@ -2,11 +2,12 @@ require 'minitest/autorun'
 class StringSum
   def self.add(data)
     return 0 if data.empty?
-    return data.to_i if data.length == 1
+    data.split(',').map(&:to_i).sum
   end
 end
 puts StringSum.add("") # => 0
 puts StringSum.add("5") # => 5
+puts StringSum.add("3,5") # => 8
 class StringSumTest < Minitest::Test
   # Test for adding with an empty string.
   def test_addition_with_empty_string
@@ -16,4 +17,9 @@ class StringSumTest < Minitest::Test
   def test_addition_with_one_number
     assert_equal 2, StringSum.add("2")
   end
+  # Test for adding with two digit numbers.
+  def test_addition_with_two_numbers
+    assert_equal 6, StringSum.add("2,4")
+  end
+
 end
