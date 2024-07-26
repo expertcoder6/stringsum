7d1680bf6f19045ff36be20dbe5462dfb0b448c1
expertcoder <expertcoder6@gmail.com>
2024-07-26 17:30:50 +0530

    Adding  test case for multiple values

--- string_sum.rb
+++ string_sum.rb
@@ -9,6 +9,7 @@ puts StringSum.add("") # => 0
 puts StringSum.add("5") # => 5
 puts StringSum.add("3,5") # => 8
 puts StringSum.add("5\n4,8") # => 17
+puts StringSum.add("10,20,30") # => 60
 class StringSumTest < Minitest::Test
   # Test for adding with an empty string.
   def test_addition_with_empty_string
@@ -25,6 +26,10 @@ class StringSumTest < Minitest::Test
   # Test for adding with a mix of comma and newline separated numbers.
   def test_addition_with_mixed_separators
     assert_equal 23, StringSum.add("5,7\n3,8")
+  end
+  # Test for adding with multiple numbers.
+  def test_addition_with_multiple_numbers
+    assert_equal 30, StringSum.add("4,5,6,7,8")
   end
 
 end
